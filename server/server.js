db78d1582345b78f63756356029ebb0b439cac9f
root <root@localhost.localdomain>
2018-01-22 22:15:53 -0500

    Add connect and disconnect event handler

--- server/server.js
+++ server/server.js
@@ -1,19 +1,33 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-const publicPath = path.join(__dirname,'../public');
+const socketIO = require ('socket.io');
 
+const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 //__dirname est le 'path' 
 //console.log (__dirname + '/../public');
 //console.log (publicPath);
 
 var app = express();
+//app.listen est la meme, mais celle la est explicite 
+var server = http.createServer(app);
+//listen to even with this
+var io = socketIO(server);
 //to configue express static middleware
 //ce qui veut dire je peux servir dans mon port a cette publicpath 
 //....tous les fichiers a l'interieur de ce repertoire
-
 app.use(express.static(publicPath));
-app.listen (port, ()=>{
+//listen to even connection
+io.on('connection', (socket)=>{
+	console.log('New user connected');
+	socket.on('disconnect', ()=>{
+		console.log ('User was disconnected');
+	});
+});
+
+
+server.listen (port, ()=>{
 	console.log (`Server is up on ${port}`);
 });
 
